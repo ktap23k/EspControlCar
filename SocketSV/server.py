e94ca87f2387d8350b5049291bc649acb83a421a
ktap23k <ktap23k@gmail.com>
2023-05-15 10:28:17 +0700

    #change data send to esp

--- SocketSV/server.py
+++ SocketSV/server.py
@@ -91,8 +91,8 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     try:
         while True:
             data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(websocket, f"Client #{client_id} says: {data}")
+            # await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(websocket, '{'+'"data": "{}"'.format(data)+'}')
     except WebSocketDisconnect:
         manager.disconnect(websocket)
         await manager.broadcast(f"Client #{client_id} left the chat")
